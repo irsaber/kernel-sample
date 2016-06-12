d11ed46f1bd824aac01a69dc1aa372b9e7adad10
Saber Rezvani <irsaber@gmail.com>
2016-06-12 17:58:07 +0430

    add question about jiffies, why it doesn't update. git add jiffie.c !

--- jiffie.c
+++ jiffie.c
@@ -4,8 +4,15 @@
 #include <linux/timer.h>
 unsigned long js, je, tet;
 int netblock_init_module(void){
+	printk("==================");
+	printk("Module started\n");
+	//these 2 jiffes has the same value, I think we have to find sth more
+	//finer than jiffies!!! still don't know the answer
 	    js = jiffies;
 	    printk("\n[Jiffies start Time : %lu] \nModule Started.\n", js);
+	    int i=0;
+	    for (i=0; i < 100; i++)
+		    printk("i: %d", i);
 	    js = jiffies;
 	    printk("\n[Jiffies start Time 2 : %lu] \nModule Started.\n", js);
 	    return 0;
